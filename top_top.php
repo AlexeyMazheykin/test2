a8cb5648215eb6e28b15b525d4ff64593d04bca1
Alexey <mazheykin@icloud.com>
2021-06-01 20:58:18 +0300

    xzc xczxc

---
+++ top_top.php
@@ -0,0 +1,143 @@
+<?php
+
+?>
+<div class="top-panel-custom-flex-menu">
+    <div class="menu-header-module">
+        <ul class="menu-header-module-item">
+            <li id="menu-header-module-items">
+                <a class="menu-header-module-link" href="/projects/">Проекты</a>
+                <ul class="menu-header-module-item-ul"
+                    style="display: none; height: 0px; padding-top: 0px; margin-top: 0px; padding-bottom: 0px; margin-bottom: 0px;">
+                    <div class="menu-header-module-block row">
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://blizzard-vac.dastrussia.ru/" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/blizzard.jpeg"></a>
+                            </div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Встроенные пылесосы для системы центрального пылеудаления немецкой марки Blizzard-lufttechnik</span>
+                                </p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://zehnder-comfosystems.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/Zehnder.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Компактные высокоэффективные вентиляционные системы с рекуперацией тепла zehnder comfosyste</span>
+                                </p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://wolf-cwl.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/wolf.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Приточно-вытяжная система центральной вентиляции с рекупераций тепла Wolf</span><br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://cyclovac.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/cyclovac.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Встроенные пылесосы Cyclovac (Канада)</span><br>
+                                    <br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://vacuflo.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/vacuflo.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Встроенный пылесосы Vacuflo (США)</span><br>
+                                    <br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://beam.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/beam.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Встроенные пылесосы Beam electrolux (США)</span><br>
+                                    <br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="https://blizzard-ventilation.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/blizzardn.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Центральная вентиляция с рекуперацией тепла Blizzard-lufttechnik</span><br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="http://ventpanel.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/Проветриватели.jpeg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Децентрализованная вентиляция</span><br>
+                                    <br>
+                                    &nbsp;</p>
+
+                            </div>
+
+                        </div>
+                        <div class="menu-header-module-block-item col-xs-2">
+                            <div class="menu-header-module-block-item-img">
+                                <a href="https://blauberg.dastrussia.ru" target=" _blank"><img
+                                            src="/bitrix/templates/whatasoft_shop_s1/images/Blauberg-logo-20187.jpg">
+                                </a></div>
+                            <div class="menu-header-module-block-item-descr">
+                                <p><span style="font-size:11px;">Приточно-вытяжная вентиляция с рекуперацией тепла Blauberg</span><br>
+                                    &nbsp;</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </ul>
+
+
+            </li>
+        </ul>
+    </div>
+    <ul class="menu-header clear">
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/">О нас</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/usloviya_oplati/">Оплата</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/usloviya-dostavki/">Доставка</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/usloviya-vozvrata/">Возврат</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/sotrudnichestvo/">Кооперация</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/certificates/">Сертификаты</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/brends/">Бренды</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/info/fz152/">ФЗ-152</a></li>
+        <li class="menu-header-item"><a class="menu-header-link" href="/contacts/">Контакты</a></li>
+    </ul>
+</div>
+
+
